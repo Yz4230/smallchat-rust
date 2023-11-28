7ab136c4e841e0c84c07f727d562b4f8f78ff83e
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2023-11-29 02:42:08 +0900

    Implement server

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,19 +1,15 @@
 use std::io::prelude::*;
+use std::os::fd::AsRawFd;
 use std::{
     io::BufReader,
     net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream},
 };
 
+use common::ReadPoller;
+
 fn create_tcp_server(port: u16) -> TcpListener {
     let sa = SocketAddrV4::new(Ipv4Addr::LOCALHOST, port);
     TcpListener::bind(sa).unwrap()
-}
-
-fn accept_client(server_socket: &TcpListener) -> TcpStream {
-    let (stream, _) = server_socket.accept().unwrap();
-    stream.set_nonblocking(true).unwrap();
-    stream.set_nodelay(true).unwrap();
-    stream
 }
 
 struct Client {
@@ -32,27 +28,90 @@ fn main() {
         clients: Vec::new(),
     };
 
+    let mut poller = ReadPoller::new();
+
     loop {
-        let client_stream = accept_client(&chat_state.listener);
-        let mut reader = BufReader::new(&client_stream);
-
-        // first line is the nickname, format: user:<nickname>
-        let mut line = String::new();
-        reader.read_line(&mut line).unwrap();
-
-        let mut nickname = line.trim();
-        if !nickname.starts_with("user:") {
-            panic!("Invalid nickname: {}", nickname);
+        poller.add_read(&chat_state.listener);
+        for client in &chat_state.clients {
+            poller.add_read(&client.stream);
         }
-        nickname = nickname.trim_start_matches("user:");
+        poller.poll().unwrap();
 
-        let client = Client {
-            stream: client_stream,
-            nickname: nickname.to_string(),
-        };
+        if poller.is_readable(&chat_state.listener) {
+            let (stream, _) = chat_state.listener.accept().unwrap();
+            let mut reader = BufReader::new(&stream);
 
-        let ip = client.stream.peer_addr().unwrap().ip().to_string();
-        println!("New client: {} ({})", client.nickname, ip);
-        chat_state.clients.push(client);
+            let mut line = String::new();
+            reader.read_line(&mut line).unwrap();
+
+            let mut nickname = line.trim();
+            if !nickname.starts_with("user:") {
+                panic!("Invalid nickname: {}", nickname);
+            }
+            nickname = nickname.trim_start_matches("user:");
+
+            let mut client = Client {
+                stream,
+                nickname: nickname.to_string(),
+            };
+
+            let welcome_msg = "Welcome to Simple Chat! Use /nick <nick> to set your nick.\n";
+            client.stream.write_all(welcome_msg.as_bytes()).unwrap();
+
+            println!("Connected client {}", client.nickname);
+            chat_state.clients.push(client);
+        }
+
+        let mut clients_to_remove = Vec::new();
+        for i in 0..chat_state.clients.len() {
+            let (before, mid_after) = chat_state.clients.split_at_mut(i);
+            let (client, after) = mid_after.split_first_mut().unwrap();
+
+            if !poller.is_readable(&client.stream) {
+                continue;
+            }
+
+            let mut reader = BufReader::new(&client.stream);
+            let mut line = String::new();
+            let nread = reader.read_line(&mut line).unwrap();
+            if nread <= 0 {
+                // disconnect
+                println!(
+                    "Disconnected client fd={}, nick={}",
+                    client.stream.as_raw_fd(),
+                    client.nickname
+                );
+                clients_to_remove.push(i);
+                continue;
+            }
+
+            line = line.trim().to_string();
+
+            if line.starts_with("/") {
+                line = line.trim_start_matches("/").to_string();
+                if line.starts_with("nick") {
+                    let nickname = line.trim_start_matches("nick ").trim();
+                    client.nickname = nickname.to_string();
+                } else {
+                    let msg = format!("Unknown command: {}\n", line.trim());
+                    client.stream.write_all(msg.as_bytes()).unwrap();
+                }
+            } else {
+                let msg = format!("{}> {}", client.nickname, line.trim());
+                println!("{}", msg);
+                for other_client in before.iter_mut().chain(after.iter_mut()) {
+                    let msg = format!("{}\n", msg);
+                    other_client.stream.write_all(msg.as_bytes()).unwrap();
+                }
+            }
+        }
+
+        let mut removed = 0;
+        for i in clients_to_remove {
+            chat_state.clients.remove(i - removed);
+            removed += 1;
+        }
+
+        poller.clear();
     }
 }
